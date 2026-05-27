c00c701252a29958b2345a48edd785df1ee072f7
0130 <lovenvibest@gmail.com>
2026-05-27 16:43:09 +0800

    update

--- main.py
+++ main.py
@@ -9,53 +9,30 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 
-def base():
-    # bi-gram use previous one char to predict next one
-    words = open("names.txt", "r").read().lower().splitlines()
-    chars = sorted(list(set("".join(words))))
-    stoi = {ch: idx + 1 for idx, ch in enumerate(chars)}  # character -> idx
-    stoi["."] = 0  # just tricky way to avoid all zero row/col
-    cnt = len(stoi)
-    N = torch.zeros((cnt, cnt), dtype=torch.int32)
-    for w in words:
-        w = "." + w + "."
-        for ch1, ch2 in zip(w, w[1:]):
-            idx1 = stoi[ch1]
-            idx2 = stoi[ch2]
-            N[idx1, idx2] += 1
-    # model smoothing
-    P = (N + 1).float()  # -> model param
-    P /= P.sum(
-        1, keepdim=True
-    )  # broadcast rule [27 * 27] / [27 * 1] -> here broadcast is necessary
-    seed = 2147483647
-    g = torch.Generator().manual_seed(seed)
-    idx = 0  # starts from '.'
-    while True:
-        p = P[idx]
-        num_samples = 1
-        # replacement = True -> 有放回抽样
-        sample_idx = int(
-            torch.multinomial(p, num_samples, replacement=True, generator=g).item()
-        )
-        if sample_idx == 0:
-            break
-        idx = sample_idx
+class Linear:
+    def __init__(self, in_features, out_features, useBias=False):
+        self.weight = torch.randn(in_features, out_features) / in_features**0.5
+        self.bias = torch.zeros(out_features) if useBias else None
 
-    # use likelihood(log likelihood) to evaluate model
-    log_likelihood = 0.0
-    n = 0
-    for w in words:
-        for ch1, ch2 in zip(w, w[1:]):
-            idx1 = stoi[ch1]
-            idx2 = stoi[ch2]
-            prob = P[idx1, idx2]
-            log_likelihood += torch.log(prob)
-            n += 1
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias:
+            self.out += self.bias
+        return self.out
 
-    # minimize the loss
-    loss = -log_likelihood / n
-    print(f"loss = {loss}")
+    def parameters(self):
+        return [self.weight] + ([self.bias] if self.bias else [])
+
+
+class BatchNorm1D:
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+    def parameters(self):
+        pass
 
 
 def main():
