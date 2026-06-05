f832c737e38d7b04eda1a075104fdfd20d2769e2
0130 <lovenvibest@gmail.com>
2026-06-05 16:08:26 +0800

    feat: use Sequential

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Linear:
     def __init__(self, in_features, out_features, useBias=False):
         self.weight = torch.randn(in_features, out_features) / in_features**0.5
         self.bias = torch.zeros(out_features) if useBias else None
+        self.training = False
 
     def __call__(self, x):
         self.out = x @ self.weight
@@ -59,6 +60,9 @@ class BatchNorm1D:
 
 
 class Tanh:
+    def __init__(self):
+        self.training = False
+
     def __call__(self, x):
         self.out = torch.tanh(x)
         return self.out
@@ -70,6 +74,7 @@ class Tanh:
 class Embedding:
     def __init__(self, num_embeddings, embedding_dim):
         self.weight = torch.randn((num_embeddings, embedding_dim))
+        self.training = False
 
     def __call__(self, x):
         return self.weight[x]
@@ -79,11 +84,31 @@ class Embedding:
 
 
 class Flatten:
+    def __init__(self) -> None:
+        self.training = False
+
     def __call__(self, x):
         return x.view(x.shape[0], -1)  # x.shape[0] is batch_size
 
     def parameters(self):
         return []
+
+
+class Sequential:
+    def __init__(self, layers):
+        self.layers = layers
+        for layer in self.layers:
+            if isinstance(layer, Linear):
+                layer.weight *= 5 / 3
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        self.out = x
+        return self.out
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
 
 
 def main():
@@ -119,25 +144,24 @@ def main():
     X_tr, Y_tr = build_dataset(words[:n1])
     X_dev, Y_dev = build_dataset(words[n1:n2])
     X_te, Y_te = build_dataset(words[n2:])
-    layers = [
-        Embedding(vocab_size, embed_size),
-        Flatten(),
-        Linear(embed_size * block_size, hidden_size_1),
-        BatchNorm1D(hidden_size_1),
-        Tanh(),
-        Linear(hidden_size_1, hidden_size_1),
-        BatchNorm1D(hidden_size_1),
-        Tanh(),
-        Linear(hidden_size_1, hidden_size_1),
-        BatchNorm1D(hidden_size_1),
-        Tanh(),
-        Linear(hidden_size_1, vocab_size),
-        BatchNorm1D(vocab_size),
-    ]
-    for layer in layers:
-        if isinstance(layer, Linear):
-            layer.weight *= 5 / 3
-    parameters = [p for layer in layers for p in layer.parameters()]
+    model = Sequential(
+        [
+            Embedding(vocab_size, embed_size),
+            Flatten(),
+            Linear(embed_size * block_size, hidden_size_1),
+            BatchNorm1D(hidden_size_1),
+            Tanh(),
+            Linear(hidden_size_1, hidden_size_1),
+            BatchNorm1D(hidden_size_1),
+            Tanh(),
+            Linear(hidden_size_1, hidden_size_1),
+            BatchNorm1D(hidden_size_1),
+            Tanh(),
+            Linear(hidden_size_1, vocab_size),
+            BatchNorm1D(vocab_size),
+        ]
+    )
+    parameters = model.parameters()
     for p in parameters:
         p.requires_grad = True
     iter_num = 50000
@@ -148,9 +172,7 @@ def main():
     for i in range(iter_num):
         idx = torch.randint(0, X_tr.shape[0], (batch_size,))
         X_batch, Y_batch = X_tr[idx], Y_tr[idx]
-        x = X_batch
-        for layer in layers:
-            x = layer(x)
+        x = model(X_batch)
         loss = F.cross_entropy(x, Y_batch)
         for p in parameters:
             p.grad = None
@@ -182,10 +204,11 @@ def main():
     plt.grid(True, linestyle="--", alpha=0.5)
     plt.show()
 
+    for layer in model.layers:
+        layer.training = False
+
     # evaluate loss on dev split
-    x = X_dev
-    for layer in layers:
-        x = layer(x)
+    x = model(X_dev)
     loss = F.cross_entropy(x, Y_dev)
     print(f"loss = {loss}")
 
