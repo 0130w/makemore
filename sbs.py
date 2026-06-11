f9045444a28516fd7f1da333ebd89700cbd007dc
0130 <lovenvibest@gmail.com>
2026-06-11 12:19:32 +0800

    wip: sbs

--- sbs.py
+++ sbs.py
@@ -13,11 +13,29 @@ def draw(matrix, xlabels, ylabels):
         xticklabels=xlabels,
         yticklabels=ylabels,
     )
-    plt.title("TwoGram Probabilities", fontsize=20)
+    plt.title("BiGram Probabilities", fontsize=20)
     plt.xlabel("Second Character", fontsize=20)
     plt.ylabel("First Character", fontsize=20)
     plt.savefig("P_heatmap.png")
     plt.show()
+
+
+def sample(itos, P):
+    idx = 0
+    ctx = ""
+    seed = 123
+    num_samples = 1
+    g = torch.Generator().manual_seed(seed)
+    while True:
+        p = P[idx]
+        sample_idx = int(
+            torch.multinomial(p, num_samples, replacement=True, generator=g)
+        )
+        if sample_idx == 0:
+            break
+        else:
+            ctx += itos[sample_idx]
+            idx = sample_idx
 
 
 def main():
@@ -36,24 +54,16 @@ def main():
             N[idx1, idx2] += 1
     P = N.float()
     P = P / P.sum(1, keepdim=True)
-    seed = 123
-    num_samples = 1
-    g = torch.Generator().manual_seed(seed)
-    iter_num = 5
-    for _ in range(iter_num):
-        ctx = ""
-        idx = 0
-        while True:
-            p = P[idx]
-            sample_idx = int(
-                torch.multinomial(p, num_samples, replacement=True, generator=g)
-            )
-            if sample_idx == 0:
-                break
-            else:
-                ctx += itos[sample_idx]
-                idx = sample_idx
-        print(f"{ctx}")
+    log_P = torch.log(P)
+    loss = 0
+    for word in words:
+        word = "." + word + "."
+        for ch1, ch2 in zip(word, word[1:]):
+            idx1 = stoi[ch1]
+            idx2 = stoi[ch2]
+            loss += log_P[idx1, idx2]
+    loss = loss / len(words)
+    print(loss)
 
 
 if __name__ == "__main__":
